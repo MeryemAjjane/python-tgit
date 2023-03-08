80b4034da84cd4f2f3acdc3a2e04ee4b61b9de06
MeryemAjjane <meryem.ajjane@uit.ac.ma>
2023-03-08 13:58:10 +0100

    add req

--- test_pr.py
+++ test_pr.py
@@ -1,10 +1,11 @@
 from main import add
 from main import soustraire
+import pytest
 def test_add():
-    assert add(2, 3) == 5
-    assert add(0, 0) == 0
-    assert add(-2, 3) == 1
+    assert 2+3 == 5
+    assert 0+0 == 0
+    assert -2+3 == 1
 def test_soustraire():
-    assert soustraire(6, 3) == 3
-    assert soustraire(0, 0) == 0
-    assert soustraire(5, 3) == 2
+    assert 6-3 == 3
+    assert 0-0== 0
+    assert 5-3 == 2
