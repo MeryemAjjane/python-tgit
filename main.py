f2c09fdadbc5aeeb6a4c6e006ac6a1188ce48bc1
MeryemAjjane <meryem.ajjane@uit.ac.ma>
2023-03-10 17:16:46 +0100

    modfy

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+"""operation de base"""
+
+
 def add(a, b):
+    """addition"""
     c = a + b
     print("somme", c)
 
 
 def sous(a, b):
+    """subtraction"""
     d = a - b
     print("sous", d)
