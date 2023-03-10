ce3872a47809ec27dab2d0dcf3322eb5d1b14683
MeryemAjjane <meryem.ajjane@uit.ac.ma>
2023-03-10 15:19:55 +0100

    modfy

--- test_pr.py
+++ test_pr.py
@@ -1,9 +1,10 @@
-
 def test_add():
-    assert 2+3 == 5
-    assert 0+0 == 0
-    assert -2+3 == 1
+    assert 2 + 3 == 5
+    assert 0 + 0 == 0
+    assert -2 + 3 == 1
+
+
 def test_soustraire():
-    assert 6-3 == 3
-    assert 0-0== 0
-    assert 5-3 == 2
+    assert 6 - 3 == 3
+    assert 0 - 0 == 0
+    assert 5 - 3 == 2
